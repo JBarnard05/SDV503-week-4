385f73a9e56cea3ec655d06ffd9497b38e3ef526
JBarnard05 <Jake-Barnard@live.nmit.ac.nz>
2025-03-12 10:52:09 +1300

    Data types and notes

---
+++ app.js
@@ -0,0 +1,95 @@
+// const firstName = 'Jake'
+// const lastName = 'Barnard' 
+
+//Camel case naming
+//const classCode = 'SDV503'
+
+//invalid vars:
+//   -first-name
+//   -1_num
+//   -num_#_1
+
+//declaring diffrerent variables of different data types
+
+/** 
+let firstName = 'Jake'
+let lastName = 'Barnard'
+let country = 'South Africa'
+let age = 19
+let isStudent = true
+
+console.log(firstName, lastName, country, age, isStudent)
+*/
+
+//declaring variables with number values
+
+
+/** 
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+
+console.log(gravity, boilingPoint, PI)
+*/
+
+//variables can also be declaring in one line seperated by a comma 
+
+/**
+let thename = 'Jake', job = 'Student', live = 'Nelson'
+
+console.log(thename, job, live)
+*/
+
+// exercise 1 - variables
+
+var x, y, z, s
+
+var a = 1, b = 2, c = 3, d = 4
+
+/**
+var firstN = 'Jake',
+    LastN = 'Barnard',
+    studentStatus = 'yes',
+    currentAge = 19
+*/
+
+var firstN = 'Jake', LastN = 'Barnard', studentStatus = 'yes', currentAge = 19
+
+let myAge = 19, yourAge = 32
+
+console.log('I am', myAge, 'years old')
+console.log('You are', yourAge, 'years old')
+
+
+
+
+// TODO
+/**
+Data types:
+ 
+1-  Primitive date types
+2-  Non-primitive data types (Object references)
+
+//SECTION - Primitive data types:
+Means primitive data types are immutable data types.
+1- Numbers (7 or 7.1)
+2- Strings (Any data under a single or double quote)
+3- Boolean (true or false)
+4- Null (Empty value or no value)
+5- Undefined (a declared variable without a value)
+
+//SECTION - Object References:
+Means Object References are mutable.
+1- Objects
+2- Functions
+3- Arrays  
+Ex: let nums = [1,2,3]
+    console.log(nums)
+    nums[2] = 10 
+    console.log(nums)
+ */
+
+    let nums = [1,2,3]
+    console.log(nums)
+    nums[2] = 10 
+    console.log(nums)
